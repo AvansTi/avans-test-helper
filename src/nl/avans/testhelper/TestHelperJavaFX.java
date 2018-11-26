781e11796ce305ef0be7a8d73ac860510cb439c1
Etiënne Goossens <psm.goossens@avans.nl>
2018-11-26 22:28:12 +0100

    Added tests for looping inside all ui elements

--- src/nl/avans/testhelper/TestHelperJavaFX.java
+++ src/nl/avans/testhelper/TestHelperJavaFX.java
@@ -2,7 +2,12 @@ package nl.avans.testhelper;
 
 import fi.helsinki.cs.tmc.edutestutils.Reflex;
 import javafx.application.Application;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.fail;
 
@@ -21,7 +26,36 @@ public class TestHelperJavaFX {
         try {
             Reflex.reflect(classType).method("start").returningVoid().taking(Stage.class).invokeOn(object, stage);
         } catch (Throwable ex) {
-            fail("Cannot use methode start. Error:" + ex.getMessage());
+            fail("Cannot use method start. Error: " + ex.getMessage());
+        }
+    }
+
+    /**
+     * Find all user components of a specific type
+     * @param root Parent element
+     * @param type Type of element to search for
+     * @param <T> Type of element to search for
+     * @return List of all matched types found under the parent
+     */
+    public static <T extends Node> List<T> getAllNodesOfType(Parent root, Class type) {
+        List<T> nodes = new ArrayList<T>();
+        addAllDescendentsNodes(root, nodes, type);
+        return nodes;
+    }
+
+    /**
+     * Internal method to do recurisive search of UI compoenents
+     * @param parent Parent element
+     * @param nodes List of nodes from type T
+     * @param type Type of element to search for
+     * @param <T> Type of element to search for
+     */
+    private static <T extends Node> void addAllDescendentsNodes(Parent parent, List<T> nodes, Class type) {
+        for (Node node : parent.getChildrenUnmodifiable()) {
+            if (node.getClass().isAssignableFrom(type))
+                nodes.add((T)node);
+            if (node instanceof Parent)
+                addAllDescendentsNodes((Parent)node, nodes, type);
         }
     }
 
